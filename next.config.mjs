14ed4ee3456c68717717a2714d6a6e53477f55aa
Rohit Devhare <rohitdevhare400@gmail.com>
2024-08-27 16:32:04 +0530

    next config updated for payu configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
-        domains: ['res.cloudinary.com'],
+        domains: ['res.cloudinary.com'], // Allow images from Cloudinary
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                crypto: false,
+            };
+        }
+        return config;
+    },
+    env: {
+        PAYU_KEY: 'e3ks2w',
+        PAYU_SALT: 'Pm3xvJxIOI8npXyxWJgwFwIIwEQKsVAm',
     },
 };
 
