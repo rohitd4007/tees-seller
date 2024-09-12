1e47053781e398ea903e7cf1818de237cdcebc5f
Rohit Devhare <rohitdevhare400@gmail.com>
2024-09-12 17:22:52 +0530

    updated ui

--- src/app/productDetails/page.js
+++ src/app/productDetails/page.js
@@ -1,7 +1,9 @@
+import NavBar from "@/components/navBarComponent/navBar";
 import ProductDetail from "@/components/productDetailsComponent/productDetails";
 
 export default function ProductDetails() {
     return <>
+        <NavBar />
         <ProductDetail />
     </>;
 }
